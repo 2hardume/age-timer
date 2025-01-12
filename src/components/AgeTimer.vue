5c679204c73adfc469d2b82ec467834d634c2da4
2hardume <vlad.davydov@internet.ru>
2025-01-12 12:38:50 +0700

    add edit date timer button

--- src/components/AgeTimer.vue
+++ src/components/AgeTimer.vue
@@ -31,7 +31,9 @@ const nullTimer = {
 }
 
 const timer = reactive(nullTimer)
-const formData = ref({})
+const formData = ref({
+  birthday: isNaN(date) ? null : date.toISOString().slice(0, 10)
+})
 
 const saveBirthday = () => {
   if (!formData.value.birthday?.length) {
@@ -41,6 +43,7 @@ const saveBirthday = () => {
   const date = new Date(formData.value.birthday)
   localStorage.birthday = date.getTime()
   birthday.setTime(date.getTime())
+  updateDiff()
   toggleVisibility()
 }
 
@@ -84,7 +87,12 @@ onUnmounted(() => {
       </form>
     </div>
     <div class="timer" v-else>
-      <h1 class="age-label">AGE</h1>
+      <div class="age-label">
+        <h1>AGE</h1>
+        <div @click="toggleVisibility" style="display: inline;">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>
+        </div>
+      </div>
       <h2 class="count">{{ timer.year }}<sup>.{{ timer.ms }}</sup></h2>
     </div>
   </div>
@@ -104,10 +112,33 @@ onUnmounted(() => {
 }
 
 .age-label {
+  margin: 0 0 0 2px;
+  position: relative;
+  z-index: 10;
+  display: flex;
+  align-items: center;
+}
+
+.age-label h1 {
+  margin: 0;
   color: #B0B5B9;
   font-size: 1.2rem;
   line-height: 1;
-  margin: 0 0 0 2px;
+}
+
+.age-label svg {
+  width: 1rem;
+  height: 1rem;
+  margin-left: 0.5rem;
+  color: #B0B5B9;
+
+  cursor: pointer;
+  opacity: 0;
+  transition: .5s ease;
+}
+
+.age-label:hover svg {
+    opacity: 1;
 }
 
 .count {
